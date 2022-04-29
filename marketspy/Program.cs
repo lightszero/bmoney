ebd81463e0b39b1c66175ce39660ebedadc1ce2d
lights <lightsever@hotmail.com>
2022-04-29 15:16:08 +0800

    准备一个行情收集程序

---
+++ marketspy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace marketspy
+{
+    internal class Program
+    {
+        static store store = new store();
+        static void Main(string[] args)
+        {
+            store.Open();
+            Console.WriteLine("Hello World!");
+
+            store.Clear();
+
+            var day = new MarketDay();
+            day.day = DateTime.Now;
+            day.records = new Record[60 * 24];
+            store.WriteDay(day);
+
+            var start = store.GetStartTime();
+            var d = store.GetDayData(DateTime.Now);
+            while(true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
