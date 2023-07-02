8b0eb67e24129bb1b4cd026295567d6e0327a494
lights <lightsever@hotmail.com>
2023-07-02 20:10:33 +0800

    移除没用的random 数据函数

--- bmoney/waiter/Program.cs
+++ bmoney/waiter/Program.cs
@@ -90,22 +90,22 @@ namespace BMoney
             HtmlView.Show(pool);
 
         }
-        static void TestCandlePool_Random()
-        {
-            CandlePool pool = new CandlePool("tpool", TimeSpan.FromMinutes(5));
-            //注册一个新的指标，必须在push data之前
-            pool.RegIndicator(Indicator.IndicatorFactory.Create("KDJ", new string[] { "9", "3", "3" }));
-            //pool.RegIndicator(Indicator.IndicatorFactory.Create("EMA", null));
-            pool.RegIndicator(Indicator.IndicatorFactory.Create("MACD", null));
-            //随便喂点数据进去，每喂一个数据，都会计算所有注册的指标
-            CandleUtil.PushRandomData(pool, DateTime.Now, TimeSpan.FromMinutes(5), 10000);
+        //static void TestCandlePool_Random()
+        //{
+        //    CandlePool pool = new CandlePool("tpool", TimeSpan.FromMinutes(5));
+        //    //注册一个新的指标，必须在push data之前
+        //    pool.RegIndicator(Indicator.IndicatorFactory.Create("KDJ", new string[] { "9", "3", "3" }));
+        //    //pool.RegIndicator(Indicator.IndicatorFactory.Create("EMA", null));
+        //    pool.RegIndicator(Indicator.IndicatorFactory.Create("MACD", null));
+        //    //随便喂点数据进去，每喂一个数据，都会计算所有注册的指标
+        //    CandleUtil.PushRandomData(pool, DateTime.Now, TimeSpan.FromMinutes(5), 10000);
 
-            data.CSVImporter.SaveToCSV("data1.csv", pool);
+        //    data.CSVImporter.SaveToCSV("data1.csv", pool);
 
-            pool.Dump();
+        //    pool.Dump();
 
-            HtmlView.Show(pool);
-        }
+        //    HtmlView.Show(pool);
+        //}
     }
 
 }
